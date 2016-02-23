9244c5bb3303fcf1e20f4c6e01cc0fc8f97b9ac7
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-02-23 18:06:14 +0800

    Add GetRevision func.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"log"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"strings"
 )
 
-func RevisionMiddleware() gin.HandlerFunc {
+func GetRevision(fileName string) (string, error) {
 	// Revision file contents will be only loaded once per process
-	data, err := ioutil.ReadFile("REVISION")
+	data, err := ioutil.ReadFile(fileName)
 
 	// If we cant read file, just skip to the next request handler
 	// This is pretty much a NOOP middlware :)
 	if err != nil {
-		return func(c *gin.Context) {
-			c.Next()
-		}
+		log.Fatalf("Unable to read config file '%s'", fileName)
+
+		return "", err
 	}
 
 	// Clean up the value since it could contain line breaks
-	revision := strings.TrimSpace(string(data))
+	return strings.TrimSpace(string(data)), err
+}
+
+func RevisionMiddleware() gin.HandlerFunc {
+	revision, err := GetRevision("REVISION")
+
+	if err != nil {
+		return func(c *gin.Context) {
+			c.Next()
+ 		}
+	}
 
 	// Set out header value for each response
 	return func(c *gin.Context) {
